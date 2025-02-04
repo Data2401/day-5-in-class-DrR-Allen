7cf243c70aa8f4ed3c26942980d28265101807dd
Rudolph Allen <rudolph.allen23@gmail.com>
2025-02-04 13:34:15 -0600

    Class Assignment Parts 1 & 2

--- InClassDay5.R
+++ InClassDay5.R
@@ -15,7 +15,7 @@
 # Assign your name to a variable called `my_name`
 
 my_name <- 'Rudolph Allen'
-my_name
+
 
 # Assign the number of pets you have to a variable called `num_pets`
 
@@ -37,7 +37,7 @@ radius <- 7
 
 
 area <- pi * radius^2
-area
+
 
 
 # Create a boolean variable `too_big` that is set to TRUE if the area is larger than 100
@@ -51,31 +51,26 @@ too_big <- TRUE
 # Display the variable `my_name`
 # Display the variable `area` 
 # Display the variable `too_big`
+print(my_name)
+print(area)
 
 
 ### PART TWO ###
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name, using the variable you made above. (use the `nchar()` function)
-
-
 name_length <- nchar(my_name)
-name_length
-
-
-
+print(name_length)
 # Print the number of letters in your name
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
-
 now_doing <- paste(my_name, "is programming! ", sep = " _ ")
-now_doing
-
+print(now_doing)
 
 # Make the `now_doing` variable upper case
 
-toupper(now_doing)
+print(toupper(now_doing))
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
@@ -84,9 +79,8 @@ toupper(now_doing)
 fav_1 <- 7
 fav_2 <- 5
 
-
-fav_1/sqrt(201)
-fav_2/sqrt(201)
+fav_1 <- fav_1/sqrt(201)
+fav_2 <- fav_2/sqrt(201)
 
 
 # Divide each number by the square root of 201 and save the new value in the
@@ -95,13 +89,18 @@ fav_2/sqrt(201)
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
 
+raw_sum <- sum(fav_1, fav_2)
+print(raw_sum)
+
 # Create a variable `round_sum` that is the `raw_sum` rounded to 2 decimal places.
 # Use the `round()` function.
+round_sum <- round(raw_sum, 2)
 
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 2 decimal places
 
 # Create a variable `sum_round` that is the sum of the rounded values
+sum_round <- sum(round_1, round_2)
 
 # Print out both variables and compare. Are they the same?
-
+print(round_2 == round_1)
